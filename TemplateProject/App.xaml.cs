8a924764b8befcbbd6cdb4077151e1032c10af3b
real059mattd <real059mattd@gmail.com>
2025-04-18 09:43:36 -0500

    First push I think

--- TemplateProject/App.xaml.cs
+++ TemplateProject/App.xaml.cs
@@ -1,15 +1,14 @@
-﻿namespace TemplateProject
-{
-    public partial class App : Application
-    {
-        public App()
-        {
-            InitializeComponent();
-        }
+﻿namespace TemplateProject;
 
-        protected override Window CreateWindow(IActivationState? activationState)
-        {
-            return new Window(new AppShell());
-        }
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+    }
+
+    protected override Window CreateWindow(IActivationState? activationState)
+    {
+        return new Window(new AppShell());
     }
 }
